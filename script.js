8cf2d9a6d71610471325d14d4db49117fd6aa218
Herbert Ferreira da Silva azevedo <112709078+kakarotofla@users.noreply.github.com>
2022-11-17 12:08:58 -0300

    implementação de DOM

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleClick (){
+    document.querySelector('#titulo').innerHTML = 'Flamengo' 
+    }
+
+    function handleMove(){
+    document.querySelector('#titulo').innerHTML = 'Flamengo' 
+    }
+
+    function handleOut(){
+    document.querySelector('#titulo').innerHTML = 'Herbert Ferreira' 
+    }
